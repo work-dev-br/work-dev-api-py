e37403a2b3768d40ec2503fda033f26b24ce4c18
work-dev-br <workdevbr@gmail.com>
2020-09-13 22:45:39 -0300

    complemento 1

--- application.py
+++ application.py
@@ -1,6 +1,70 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World Nivaldo!"
+def root():
+    try:
+        data = open('index.html').read()    
+        return data
+    except:
+        return geraResponse(400, "Não foi possível abrir o arquivo index.html.")
+
+@app.route("/groups", methods=["GET"])
+def groups():
+    groupsList = { "groups": [
+        { 
+            "id": 1,
+            "name": "tips",
+            "title": "Dicas",
+            "tools": [1, 2, 3]
+        },
+        { 
+            "id": 2,
+            "name": "tools",
+            "title": "Ferramentas",
+            "tools": [1, 2, 4]
+        },
+        { 
+            "id": 3,
+            "name": "projects",
+            "title": "Projetos",
+            "tools": [1, 3, 4]
+        },
+        { 
+            "id": 4,
+            "name": "varieties",
+            "title": "Variedades",
+            "tools": []
+        },
+    ] }
+    return geraResponse( 200, "Consulta concluída com sucesso.", groupsList ) 
+
+@app.route("/tools", methods=["GET"])
+def tools():
+    toolsList = { "tools": [
+        { 
+            "id": 1,
+            "name": "reactjs",
+            "title": "React JS"
+        },
+        { 
+            "id": 2,
+            "name": "nodejs",
+            "title": "Node JS"
+        },
+        { 
+            "id": 3,
+            "name": "python",
+            "title": "Python"
+        },
+    ] }
+    return geraResponse( 200, "Consulta concluída com sucesso.", toolsList ) 
+
+def geraResponse(status, message, content=False):
+    response = {}
+    response["status"] = status
+    response["message"] = message
+    if (content):
+        response["content"] = content
+    return response
